65bcfe5a09274d5fad96b6aec9c9a149bb22a2e7
Corey Wilson <corey@commercesignals.com>
2017-02-13 16:36:53 -0500

    associate a post with a comment

--- app/Http/Controllers/Comments.php
+++ app/Http/Controllers/Comments.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
+use App\Post;
 
 class Comments extends Controller
 {
     public function create(Request $request) {
+        return Post::findOrFail($request->get('postId'))
+          ->comments()
+          ->create([
+            'body' => $request->get('body'),
+            'user_id' => $request->user()->id
+          ]);
     }
 }
