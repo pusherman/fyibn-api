dfd051d734e8066b315ac65e6a4462d6041d22fb
Corey Wilson <corey@commercesignals.com>
2017-02-22 10:50:05 -0500

    add ability to store “favorites”

--- app/Http/Controllers/Favorites.php
+++ app/Http/Controllers/Favorites.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class Favorites extends Controller
 {
-    //
+    public function create(Request $request) {
+        return Post::findOrFail($request->get('post_id'))
+          ->favorites()
+          ->create([ 'user_id' => $request->user()->id ]);
+    }
 }
