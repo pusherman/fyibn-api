009fe60eb7bf29e6374955504dd49af89c8fbbb9
Corey Wilson <corey@commercesignals.com>
2017-02-27 10:34:22 -0500

    return the favorite_id and post_id of the favorite we are deleting from the db

--- app/Http/Controllers/Favorites.php
+++ app/Http/Controllers/Favorites.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Favorite;
 
 class Favorites extends Controller
 {
@@ -20,9 +21,14 @@ class Favorites extends Controller
         $post_id = $request->get('post_id');
         $user_id = $request->user()->id;
 
-        return Post::findOrFail($post_id)
-          ->favorites()
+        $favorite_id = Favorite::findOrFail($post_id)
+          ->where('post_id', '=', $post_id)
           ->where('user_id', '=', $user_id)
           ->delete();
+
+        return [
+          'post_id' => $post_id,
+          'favorite_id' => $favorite_id
+        ];
     }
 }
